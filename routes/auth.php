bcb569489934fd0af0d43c0c1c4119422b3d7faa
MAZAVAMANANA Zeppélin Tiavy <zeppelintiavymazavamanana@gmail.com>
2026-03-02 13:32:36 +0300

    Zeppélin

--- routes/auth.php
+++ routes/auth.php
@@ -12,10 +12,10 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
+    Route::get('zeppélin/obaya/angilio', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    Route::post('zeppélin/obaya/angilio', [AuthenticatedSessionController::class, 'store']);
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
         ->name('password.request');
